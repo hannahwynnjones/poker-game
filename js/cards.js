3fa2986e50b3b6e974cb41ba4aef7d9e599aedf5
Hannah Wynn Jones <hannahwynnjones@hotmail.co.uk>
2017-05-24 18:43:06 +0100

    initial commit, 2 player 5 card draw, no AJQK

---
+++ js/cards.js
@@ -0,0 +1,316 @@
+//would like to use these instead of const names and const suits
+
+// var cards = [
+//   {
+//     name: 'Ace',
+//     suit: 'clubs',
+//     points: 1,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Two',
+//     suit: 'clubs',
+//     points: 2,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Three',
+//     suit: 'clubs',
+//     points: 3,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Four',
+//     suit: 'clubs',
+//     points: 4,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Five',
+//     suit: 'clubs',
+//     points: 5,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Six',
+//     suit: 'clubs',
+//     points: 6,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Seven',
+//     suit: 'clubs',
+//     points: 7,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Eight',
+//     suit: 'clubs',
+//     points: 8,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Nine',
+//     suit: 'clubs',
+//     points: 9,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Ten',
+//     suit: 'clubs',
+//     points: 10,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Jack',
+//     suit: 'clubs',
+//     points: 11,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Queen',
+//     suit: 'clubs',
+//     points: 12,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'King',
+//     suit: 'clubs',
+//     points: 13,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Ace',
+//     suit: 'diamonds',
+//     points: 1,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Two',
+//     suit: 'diamonds',
+//     points: 2,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Three',
+//     suit: 'diamonds',
+//     points: 3,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Four',
+//     suit: 'diamonds',
+//     points: 4,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Five',
+//     suit: 'diamonds',
+//     points: 5,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Six',
+//     suit: 'diamonds',
+//     points: 6,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Seven',
+//     suit: 'diamonds',
+//     points: 7,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Eight',
+//     suit: 'diamonds',
+//     points: 8,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Nine',
+//     suit: 'diamonds',
+//     points: 9,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Ten',
+//     suit: 'diamonds',
+//     points: 10,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Jack',
+//     suit: 'diamonds',
+//     points: 11,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Queen',
+//     suit: 'diamonds',
+//     points: 12,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'King',
+//     suit: 'diamonds',
+//     points: 13,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Ace',
+//     suit: 'spades',
+//     points: 1,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Two',
+//     suit: 'spades',
+//     points: 2,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Three',
+//     suit: 'spades',
+//     points: 3,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Four',
+//     suit: 'spades',
+//     points: 4,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Five',
+//     suit: 'spades',
+//     points: 5,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Six',
+//     suit: 'spades',
+//     points: 6,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Seven',
+//     suit: 'spades',
+//     points: 7,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Eight',
+//     suit: 'spades',
+//     points: 8,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Nine',
+//     suit: 'spades',
+//     points: 9,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Ten',
+//     suit: 'spades',
+//     points: 10,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Jack',
+//     suit: 'spades',
+//     points: 11,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Queen',
+//     suit: 'spades',
+//     points: 12,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'King',
+//     suit: 'spades',
+//     points: 13,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Ace',
+//     suit: 'hearts',
+//     points: 1,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Two',
+//     suit: 'hearts',
+//     points: 2,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Three',
+//     suit: 'hearts',
+//     points: 3,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Four',
+//     suit: 'hearts',
+//     points: 4,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Five',
+//     suit: 'hearts',
+//     points: 5,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Six',
+//     suit: 'hearts',
+//     points: 6,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Seven',
+//     suit: 'hearts',
+//     points: 7,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Eight',
+//     suit: 'hearts',
+//     points: 8,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Nine',
+//     suit: 'hearts',
+//     points: 9,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Ten',
+//     suit: 'hearts',
+//     points: 10,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Jack',
+//     suit: 'hearts',
+//     points: 11,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'Queen',
+//     suit: 'hearts',
+//     points: 12,
+//     image: 'css/images/king.png'
+//   },
+//   {
+//     name: 'King',
+//     suit: 'hearts',
+//     points: 13,
+//     image: 'css/images/king.png'
+//   }
+// ];
